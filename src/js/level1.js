2062e1d026209d5c613cd4b24da6c5f99d52968d
abelt <113996197+fefeffefeeffefefe@users.noreply.github.com>
2024-06-11 14:23:35 +0200

    camera follow player

--- src/js/level1.js
+++ src/js/level1.js
@@ -1,12 +1,25 @@
 import { Actor, Scene, Vector, Color, BoundingBox, Sound, Timer } from "excalibur";
 import { Resources, ResourceLoader } from './resources.js';
 import { Player } from './player.js'
+import { Background } from "./background.js";
 
 export class Level extends Scene {
 
     onActivate(){
+        this.background = new Background(Resources.WindowsHills.toSprite(), 750, 370, 1.1, 1);
+        this.add(this.background);
+
         this.player = new Player();
         this.add(this.player);
     }
+
+    onPreUpdate(engine, delta) {
+        super.onPreUpdate(engine, delta);
+        
+        const playerPos = this.player.pos;
+
+        this.camera.pos = playerPos;
+    }
+
 }
 
